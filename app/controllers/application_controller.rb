583fd34b449d432c2b6e6f174029e5e2255957a2
J Perrin <perrinjack96@gmail.com>
2020-12-08 14:13:01 +0000

    For the moment unmatched routes show not found

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,7 @@
 class ApplicationController < ActionController::API
     include Response
+
+    def not_found
+        render plain: "Not found.", status: 404
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
+  root to: "application#not_found"
+  
+
   resources :seasons do 
     resources :matches
   end
+
+  get '*unmatched_route', to: 'application#not_found'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
