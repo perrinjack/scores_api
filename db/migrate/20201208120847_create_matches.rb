8f61750596fc66aefb33324005a497894fe3b0ba
J Perrin <perrinjack96@gmail.com>
2020-12-08 12:15:11 +0000

    Created Season and Matches tables

---
+++ db/migrate/20201208120847_create_matches.rb
@@ -0,0 +1,13 @@
+class CreateMatches < ActiveRecord::Migration[6.0]
+  def change
+    create_table :matches do |t|
+      t.string :home_team
+      t.string :away_team
+      t.integer :home_team_score
+      t.integer :away_team_score
+      t.references :season, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
