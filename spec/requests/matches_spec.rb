5a745206b535c123aa089083b160a35e5397accb
J Perrin <perrinjack96@gmail.com>
2020-12-09 12:51:50 +0000

    If no Season found returns 404

--- spec/requests/matches_spec.rb
+++ spec/requests/matches_spec.rb
@@ -21,5 +21,24 @@ RSpec.describe 'Items API' do
         expect(json.size).to eq(20)
       end
     end
+
+    context 'when season does not exist' do
+        let(:season_id) { 0 }
+  
+        it 'returns status code 404' do
+          expect(response).to have_http_status(404)
+        end
+  
+        it 'returns a not found message' do
+          expect(response.body).to match(/Couldn't find Season/)
+        end
+      end
+
+
+
+
+
+
+
   end
 end
