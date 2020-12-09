daf39526eef55d8d819290a7523fc5d84cc1176e
J Perrin <perrinjack96@gmail.com>
2020-12-09 13:15:34 +0000

    Specific Match resource can be found

--- app/controllers/matches_controller.rb
+++ app/controllers/matches_controller.rb
@@ -1,10 +1,26 @@
 # frozen_string_literal: true
 
 class MatchesController < ApplicationController
+    before_action :set_season
+    before_action :set_season_item, only: [:show]
   def index
   
     @matches = Season.find(params[:season_id]).matches
 
     json_response(@matches)
   end
+
+  def show
+    @match = Season.find(params[:season_id]).matches
+    json_response(@match)
+  end
+
+  def set_season
+    @season = Season.find(params[:season_id])
+  end
+
+  def set_season_item
+    @match = @season.matches.find_by!(id: params[:id]) if @season
+  end
+
 end
--- spec/requests/matches_spec.rb
+++ spec/requests/matches_spec.rb
@@ -2,7 +2,7 @@
 
 require 'rails_helper'
 
-RSpec.describe 'Items API' do
+RSpec.describe 'Matches API' do
   # Initialize the test data
   let!(:season) { create(:season) }
   let!(:matches) { create_list(:match, 20, season_id: season.id) }
@@ -35,10 +35,31 @@ RSpec.describe 'Items API' do
       end
 
 
+  end
 
+  describe 'GET /seasons/:season_id/matches/:match_id' do
+    before { get "/seasons/#{season_id}/matches/#{id}" }
 
+    context 'when Seasons match exists' do
+      it 'returns status code 200' do
+        expect(response).to have_http_status(200)
+      end
 
+      it 'returns the match' do
+        expect(json.first['id']).to eq(id)
+      end
+    end
 
+    context 'when match does not exist' do
+      let(:id) { 0 }
 
+      it 'returns status code 404' do
+        expect(response).to have_http_status(404)
+      end
+
+      it 'returns a not found message' do
+        expect(response.body).to match(/Couldn't find Match/)
+      end
+    end
   end
 end
